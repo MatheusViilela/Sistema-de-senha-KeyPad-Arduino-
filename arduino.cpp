e9b2b9495e0558f3741c30ab284973581963c6cb
Matheus Vilela <matheusvilela30@gmail.com>
2023-05-30 22:21:40 -0300

    Definindo rotas node

--- arduino.cpp
+++ arduino.cpp
@@ -1,7 +1,7 @@
 #include <Stepper.h>
 
 #include <Servo.h>
-#include <Keypad.h>
+
 
 Servo servoMotor;
 
@@ -15,89 +15,62 @@ const int passosPorRevolucao = 200;
 
 Stepper motor(passosPorRevolucao, passoPino1, passoPino3, passoPino2, passoPino4);
 
-const byte LINHAS = 4;
-const byte COLUNAS = 4;
-
-const char TECLAS_MATRIZ[LINHAS][COLUNAS] = {
-    {'1', '2', '3', 'A'},
-    {'4', '5', '6', 'B'},
-    {'7', '8', '9', 'C'},
-    {'*', '0', '#', 'D'}};
-
-const byte PINOS_LINHAS[LINHAS] = {9, 8, 7, 6};
-const byte PINOS_COLUNAS[COLUNAS] = {5, 4, 3, 2};
-
-Keypad teclado_personalizado = Keypad(makeKeymap(TECLAS_MATRIZ), PINOS_LINHAS, PINOS_COLUNAS, LINHAS, COLUNAS);
-
-void setup()
-{
+void setup() {
   servoMotor.write(0);
   Serial.begin(9600);
-  servoMotor.attach(42); // Conecta o servo motor ao pino 42 do Arduino
+  servoMotor.attach(42);  // Conecta o servo motor ao pino 42 do Arduino
 }
-
-void loop()
-{
-
-  String palavra = "";    // Variável para armazenar a palavra
-  int contador = 0;       // Contador de dígitos
-  int contadorApagar = 0; // Contador para a tecla de apagar "#"
-
-  while (contador < 6)
-  { // Aguarda a digitação de 6 dígitos
-    char tecla = teclado_personalizado.getKey();
-
-    if (tecla)
-    { // Se alguma tecla foi pressionada
-      if (tecla == '#')
-      {
-        contadorApagar++; // Incrementa o contador para a tecla de apagar
-        if (contadorApagar == 4)
-        {                     // Se a tecla de apagar foi pressionada 4 vezes
-          palavra = "";       // Reinicia a palavra
-          contador = 0;       // Reinicia o contador de dígitos
-          contadorApagar = 0; // Reinicia o contador da tecla de apagar
-          Serial.println("Pronto para cadastrar nova senha: ");
-        }
-      }
-      else
-      {
-        palavra += tecla; // Concatena o dígito na palavra
-        contador++;       // Incrementa o contador de dígitos
-      }
-    }
-  }
-
-  Serial.println("Senha Digitada: " + palavra); // Imprime a palavra na porta serial
-  contador = 0;
-  while (contador < 1)
-  {
+int contadorAcesso = 0;
+void loop() {
+  servoMotor.write(0);
+  while (contadorAcesso < 1) {
     int c = Serial.parseInt();
-    if (c == -1)
-    {
+    if (c == -1) {
+      abrirCortina();
+      delay(1000);
+      contadorAcesso = 0;
+
+    } else if (c == -2) {
+      fecharCortina();
+      contadorAcesso = 0;
+    } else if (c == -3) {
       abrirPorta();
-      contador = 1;
-    }
-    else if (c == -2)
-    {
-      contador = 1;
+      
+      contadorAcesso = 0;
+    } else if (c == -4) {
+      fecharPorta();
+      contadorAcesso = 0;
     }
   }
 }
-void abrirPorta()
-{
-  unsigned long tempoInicial = millis();   // Obtém o tempo inicial em milissegundos
-  const unsigned long tempoLimite = 10000; // 10s
 
-  servoMotor.write(180);
-  while (true)
-  {
-    motor.setSpeed(100); // Defina a velocidade do motor (ajuste conforme necessário)
+void abrirCortina() {
+  unsigned long tempoInicial = millis();    // Obtém o tempo inicial em milissegundos
+  const unsigned long tempoLimite = 10000;  // 10s
+  while (true) {
+    motor.setSpeed(100);  // Defina a velocidade do motor (ajuste conforme necessário)
     motor.step(passosPorRevolucao);
-    if (millis() - tempoInicial >= tempoLimite)
-    {
-      break; // Saia do loop
+    if (millis() - tempoInicial >= tempoLimite) {
+      break;  // Saia do loop
     }
   }
+ 
+}
+void fecharCortina() {
+  unsigned long tempoInicial = millis();    // Obtém o tempo inicial em milissegundos
+  const unsigned long tempoLimite = 10000;  // 10s
+  while (true) {
+    motor.setSpeed(100);  // Defina a velocidade do motor (ajuste conforme necessário)
+    motor.step(-passosPorRevolucao);
+    if (millis() - tempoInicial >= tempoLimite) {
+      break;  // Saia do loop
+    }
+  }
+}
+
+void abrirPorta() {
+  servoMotor.write(180);
+}
+void fecharPorta() {
   servoMotor.write(0);
 }
--- index.js
+++ index.js
@@ -1,103 +1,126 @@
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
-const mysql = require('mysql2/promise');
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 
-const port = new SerialPort({ path: 'COM8', baudRate: 9600 });
+
+const port = new SerialPort({ path: 'COM4', baudRate: 9600 });
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
+function enviarSinal(comando) {
+  port.write(comando, (err) => {
+    if (err) {
+      console.error(`Erro ao enviar comando para abrir a porta: ${err.message}`);
+    } else {
+      console.log('Comando enviado');
+    }
+  });
+}
+// Configuração do banco de dados
 const dbConfig = {
   host: '127.0.0.1',
   user: 'root',
   password: '',
   database: 'casa',
 };
-
-console.log('Seja bem-vindo ao sistema de controle de acesso!\nDigite sua senha:');
-
-let cadastrarSenha = false;
-let novaSenha = '';
-let acessoLiberado = false;
-let fechandoCortina = false;
-
-function enviarSinal(comando) {
-  console.log(typeof port);
-
-  port.write(comando, (err) => {
-    if (err) {
-      console.error(`Erro ao enviar comando para abrir a porta: ${err.message}`);
-    } else {
-      console.log(comando);
-      console.log('Comando enviado');
-    }
-  });
-}
-
-async function saveDataToDatabase(senha) {
-  try {
-    const connection = await mysql.createConnection(dbConfig);
-    const [rows, fields] = await connection.execute(
-      'INSERT INTO controle_acesso (senha, tms_cadastro) VALUES (PASSWORD(?), NOW())',
-      [senha]
-    );
-    console.log(`Dados salvos no banco de dados: ${senha}`);
-    cadastrarSenha = null;
-  } catch (error) {
-    console.error(`Erro ao salvar dados no banco de dados: ${error.message}`);
-  }
-}
-
-async function acesso(senha) {
-  try {
-    const connection = await mysql.createConnection(dbConfig);
-    const [rows, fields] = await connection.execute(
-      'SELECT * FROM controle_acesso WHERE senha = PASSWORD(?)',
-      [senha]
-    );
-    if (rows.length > 0) {
-      console.log('Acesso Liberado');
-      acessoLiberado = true;
-      enviarSinal('-1');
-    } else {
-      console.log('Acesso Negado');
-      acessoLiberado = false;
-      enviarSinal('-2');
-    }
-  } catch (error) {
-    console.error(`Erro ao salvar dados no banco de dados: ${error.message}`);
-  }
-}
-
 parser.on('data', async function (data) {
-  console.log('Dados recebidos:', data);
-  const senhaIndex = data.indexOf('Senha Digitada:');
-  const prontoIndex = data.indexOf('Pronto para cadastrar nova senha:');
-
-  if (senhaIndex === 0) {
-    const senha = data.split(':');
-    const numero = senha[1].trim();
-    console.log(`Senha digitada: ${numero}`);
-
-    if (cadastrarSenha === false) {
-      await acesso(numero);
-    }
-
-    if (cadastrarSenha === true) {
-      novaSenha = numero;
-      cadastrarSenha = false;
-      await saveDataToDatabase(novaSenha);
-    }
-  } else if (prontoIndex === 0 && acessoLiberado === true) {
-    console.log(data);
-    cadastrarSenha = true;
-  } else {
-    console.log('Você precisa acessar para cadastrar nova senha');
-  }
 });
 
+// Criação da conexão com o banco de dados
+const connection = mysql.createConnection(dbConfig);
+
+// Conexão com o banco de dados
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão com o banco de dados estabelecida');
+});
+app.use(session({
+  secret: 'chave-secreta', // Chave secreta para assinar a sessão
+  resave: false, // Evita que a sessão seja regravada no servidor a cada requisição
+  saveUninitialized: false // Evita que uma sessão nova seja criada para cada requisição não autenticada
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Consulta no banco de dados para verificar as informações de login
+  const query = `SELECT * FROM usuarios WHERE email = ? AND senha = PASSWORD(?)`;
+  connection.query(query, [username, password], (err, results) => {
+    if (err) {
+      console.error('Erro ao executar a consulta:', err);
+      res.send('Erro ao fazer login');
+      return;
+    }
+
+
+    if (results.length > 0) {
+      req.session.username = username; // Salva o nome de usuário na sessão
+      req.session.loggedIn = true; // Marca a sessão como autenticada
+      res.redirect('/home');
+    } else {
+      // Caso as informações não correspondam, exibe uma mensagem de erro
+      res.send('<script>alert("Login ou senha invalida"); window.location.href = "/";</script>');
+    }
+  });
+});
 app.get('/', (req, res) => {
+  res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Formulário de Login</title>
+    <!-- Adicione os arquivos CSS do Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  </head>
+  <body>
+  <form action="/" method="POST">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="card mt-5">
+            <div class="card-body">
+              <h2 class="text-center">Formulário de Login</h2>
+              <form>
+                <div class="form-group">
+                  <label for="username">Usuário:</label>
+                  <input type="text" class="form-control" id="username" name="username" placeholder="Digite seu usuário">
+                </div>
+                <div class="form-group">
+                  <label for="password">Senha:</label>
+                  <input type="password" class="form-control" id="password" name="password"placeholder="Digite sua senha">
+                </div>
+                <div class="text-center">
+                  <button type="submit" class="btn btn-primary">Entrar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Adicione os arquivos JavaScript do Bootstrap -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  </body>
+  </form>
+  </html>`);
+});
+
+function requireLogin(req, res, next) {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+
+    res.send('<script>alert("Você precisa efetuar login para acessar!"); window.location.href = "/";</script>');
+  }
+}
+app.get('/home', requireLogin, (req, res) => {
   res.send(`
     <!DOCTYPE html>
     <html lang="en">
@@ -109,6 +132,7 @@ app.get('/', (req, res) => {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.5.0/dist/semantic.min.css" />
         <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.5.0/dist/semantic.min.js"></script>
+        <script src="https://unpkg.com/@phosphor-icons/web"></script>
         <link rel="stylesheet" href="styles.css" />
       </head>
       <style>
@@ -121,7 +145,7 @@ body {
     margin-right: auto !important;
     background-color: rgb(241, 93, 39) !important;
 }
-      </style>
+</style>
       <body>
         <div class="ui raised very padded text container segment">
           <h2 class="ui header">Olá! O que você deseja?</h2>
@@ -138,7 +162,7 @@ body {
           <br />
           <br />
           <div class="ui compact menu">
-            <a class="item">
+           <a class="item" href="/abrir-cortina">
               <i class="sun icon"></i>
               Abrir Cortina
             </a>
@@ -148,6 +172,18 @@ body {
             </a>
 
           </div>
+          <div class="ui compact menu">
+          <a class="item" href="/trancar-porta">
+          <i class="ph-fill ph-door"></i>
+             Trancar Porta
+           </a>
+           <a class="item" href="/abrir-porta">
+           <i class="ph-fill ph-door-open"></i>
+             Abrir a porta
+           </a>
+           </div>
+
+         </div>
         </div>
         
         <script src="script.js"></script>
@@ -155,15 +191,25 @@ body {
     </html>
   `);
 });
-
-app.get('/fechar-cortina', async (req, res) => {
-  console.log("entrei");
+app.get('/abrir-cortina', (req, res) => {
+  enviarSinal('-1');
+  res.send('<script>alert("Abrindo cortina"); window.location.href = "/home";</script>');
+});
+app.get('/fechar-cortina', (req, res) => {
+  enviarSinal('-2');
+  res.send('<script>alert("Fechando cortina"); window.location.href = "/home";</script>');
+});
+app.get('/abrir-porta', (req, res) => {
   enviarSinal('-3');
-
-  res.send('Fechando cortina...');
+  res.send('<script>alert("Porta destrancada"); window.location.href = "/home";</script>');
+});
+app.get('/trancar-porta', (req, res) => {
+  enviarSinal('-4');
+  res.send('<script>alert("Porta Trancada"); window.location.href = "/home";</script>');
 });
 
-app.listen(5000, () => {
+
+app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
 
